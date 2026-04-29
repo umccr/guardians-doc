f1f81e188a141ed4710b3ea11cd518eed17a558d
Andrew Patterson <andrew@patto.net>
2026-04-29 14:31:57 +1000

    Renamed repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://umccr.github.io',
-  base: '/guardians-docs',
+  base: '/guardians-doc',
 
   integrations: [starlight({
       title: 'UMCCR Guardians',
